055ae94ae001983d4685b118061283512b721eee
Guillaume Zeldine <g.zeldine@hotmail.fr>
2024-01-08 03:16:13 -0300

    Fixing assets stimulus prod

--- public/assets/controllers/index-bd86c7dd4182589c7bb1cb2ecfa5528ab4bfc1baa12b1eec959f37130d9a541e.js
+++ public/assets/controllers/index-bd86c7dd4182589c7bb1cb2ecfa5528ab4bfc1baa12b1eec959f37130d9a541e.js
@@ -14,4 +14,4 @@ application.register("dropdown", DropdownController)
 
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
-// lazyLoadControllersFrom("controllers", application)
+// lazyLoadControllersFrom("controllers", application);
